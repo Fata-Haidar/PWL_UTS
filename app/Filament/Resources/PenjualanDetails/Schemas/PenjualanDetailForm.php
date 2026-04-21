8e91fcdacb76acf5887e19a3f3b238ba1064465d
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 19:58:20 +0700

    fileament penjualan_detail

--- app/Filament/Resources/PenjualanDetails/Schemas/PenjualanDetailForm.php
+++ app/Filament/Resources/PenjualanDetails/Schemas/PenjualanDetailForm.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\PenjualanDetails\Schemas;
 
+use App\Models\Barang;
+use App\Models\Penjualan;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class PenjualanDetailForm
@@ -10,7 +14,29 @@ class PenjualanDetailForm
     {
         return $schema
             ->components([
-                //
+                Select::make('penjualan_id')
+                    ->label('Kode Penjualan')
+                    ->options(Penjualan::pluck('penjualan_kode', 'penjualan_id'))
+                    ->searchable()
+                    ->required(),
+
+                Select::make('barang_id')
+                    ->label('Barang')
+                    ->options(Barang::pluck('barang_nama', 'barang_id'))
+                    ->searchable()
+                    ->required(),
+
+                TextInput::make('harga')
+                    ->label('Harga')
+                    ->numeric()
+                    ->required()
+                    ->prefix('Rp'),
+
+                TextInput::make('jumlah')
+                    ->label('Jumlah')
+                    ->numeric()
+                    ->required()
+                    ->minValue(1),
             ]);
     }
 }
--- app/Filament/Resources/PenjualanDetails/Tables/PenjualanDetailsTable.php
+++ app/Filament/Resources/PenjualanDetails/Tables/PenjualanDetailsTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\PenjualanDetails\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class PenjualanDetailsTable
@@ -13,13 +15,32 @@ class PenjualanDetailsTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('penjualan.penjualan_kode')
+                    ->label('Kode Penjualan')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('barang.barang_nama')
+                    ->label('Barang')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('harga')
+                    ->label('Harga')
+                    ->money('IDR')
+                    ->sortable(),
+
+                TextColumn::make('jumlah')
+                    ->label('Jumlah')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i'),
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
