1af018aaaa7d2b6d844a2d427511235a57509416
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 22:03:20 +0700

    filament penjualan_detail

--- app/Filament/Resources/PenjualanDetails/Pages/EditPenjualanDetail.php
+++ app/Filament/Resources/PenjualanDetails/Pages/EditPenjualanDetail.php
@@ -16,4 +16,8 @@ class EditPenjualanDetail extends EditRecord
             DeleteAction::make(),
         ];
     }
+        protected function getRedirectUrl(): string
+        {
+            return $this->getResource()::getUrl('index');
+        }
 }
