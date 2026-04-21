d76ff54e365adfae373d18d8aa1eeff51c79a15d
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 21:30:13 +0700

    Model User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,60 +2,49 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
 use Filament\Panel;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser, HasName
 {
-    /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-     protected $fillable = [
+
+    protected $fillable = [
         'level_id',
         'username',
         'nama',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
-        public function canAccessPanel(Panel $panel): bool
+
+    public function canAccessPanel(Panel $panel): bool
     {
         return true;
     }
-     public function level()
+
+    public function getFilamentName(): string
+    {
+        return $this->nama ?? $this->username ?? 'User';
+    }
+
+    public function level()
     {
         return $this->belongsTo(Level::class, 'level_id', 'level_id');
     }
