ebb9a7d52a2662a2351a4022f3293ec830a32071
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 11:55:38 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
+     protected $fillable = [
+        'level_id',
+        'username',
+        'nama',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+        public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+     public function level()
+    {
+        return $this->belongsTo(Level::class, 'level_id', 'level_id');
+    }
+
+    public function stok()
+    {
+        return $this->hasMany(Stok::class, 'user_id', 'user_id');
+    }
+
+    public function penjualan()
+    {
+        return $this->hasMany(Penjualan::class, 'user_id', 'user_id');
+    }
+}
