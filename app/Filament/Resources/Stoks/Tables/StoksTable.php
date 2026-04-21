ce33fa67825910b76655dfc545803bb605885956
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 20:01:52 +0700

    filament stok

--- app/Filament/Resources/Stoks/Tables/StoksTable.php
+++ app/Filament/Resources/Stoks/Tables/StoksTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Stoks\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class StoksTable
@@ -13,13 +15,37 @@ class StoksTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('supplier.supplier_nama')
+                    ->label('Supplier')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('barang.barang_nama')
+                    ->label('Barang')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('user.nama')
+                    ->label('User')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('stok_tanggal')
+                    ->label('Tanggal')
+                    ->dateTime('d-m-Y H:i')
+                    ->sortable(),
+
+                TextColumn::make('stok_jumlah')
+                    ->label('Jumlah')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i'),
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
