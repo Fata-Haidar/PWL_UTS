7cb372877c4c945c89f433cb98396b9ce8998353
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 20:08:24 +0700

    filament user

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -2,6 +2,9 @@
 
 namespace App\Filament\Resources\Users\Schemas;
 
+use App\Models\Level;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class UserForm
@@ -10,7 +13,29 @@ class UserForm
     {
         return $schema
             ->components([
-                //
+                Select::make('level_id')
+                    ->label('Level')
+                    ->options(Level::pluck('level_nama', 'level_id'))
+                    ->searchable()
+                    ->required(),
+
+                TextInput::make('username')
+                    ->label('Username')
+                    ->required()
+                    ->maxLength(20)
+                    ->unique(ignoreRecord: true),
+
+                TextInput::make('nama')
+                    ->label('Nama')
+                    ->required()
+                    ->maxLength(100),
+
+                TextInput::make('password')
+                    ->label('Password')
+                    ->password()
+                    ->revealable()
+                    ->required()
+                    ->maxLength(255),
             ]);
     }
 }
--- app/Filament/Resources/Users/Tables/UsersTable.php
+++ app/Filament/Resources/Users/Tables/UsersTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Users\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class UsersTable
@@ -13,13 +15,29 @@ class UsersTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('username')
+                    ->label('Username')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('nama')
+                    ->label('Nama')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('level.level_nama')
+                    ->label('Level')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i')
+                    ->sortable(),
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
