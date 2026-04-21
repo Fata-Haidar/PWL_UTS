d3c424cd4849f7b9bb3df3041fb8e406bcb15de2
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 19:59:45 +0700

    filament penjualan

--- app/Filament/Resources/Penjualans/Schemas/PenjualanForm.php
+++ app/Filament/Resources/Penjualans/Schemas/PenjualanForm.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Resources\Penjualans\Schemas;
 
+use App\Models\User;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
 class PenjualanForm
@@ -10,7 +14,26 @@ class PenjualanForm
     {
         return $schema
             ->components([
-                //
+                Select::make('user_id')
+                    ->label('User / Kasir')
+                    ->options(User::pluck('nama', 'user_id'))
+                    ->searchable()
+                    ->required(),
+
+                TextInput::make('pembeli')
+                    ->label('Pembeli')
+                    ->required()
+                    ->maxLength(50),
+
+                TextInput::make('penjualan_kode')
+                    ->label('Kode Penjualan')
+                    ->required()
+                    ->maxLength(20)
+                    ->unique(ignoreRecord: true),
+
+                DateTimePicker::make('penjualan_tanggal')
+                    ->label('Tanggal Penjualan')
+                    ->required(),
             ]);
     }
 }
--- app/Filament/Resources/Penjualans/Tables/PenjualansTable.php
+++ app/Filament/Resources/Penjualans/Tables/PenjualansTable.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Penjualans\Tables;
 
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class PenjualansTable
@@ -13,13 +15,33 @@ class PenjualansTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('penjualan_kode')
+                    ->label('Kode Penjualan')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('user.nama')
+                    ->label('Kasir')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('pembeli')
+                    ->label('Pembeli')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('penjualan_tanggal')
+                    ->label('Tanggal')
+                    ->dateTime('d-m-Y H:i')
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i'),
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
