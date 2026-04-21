ddbfa46189d7ddc274325e019f6164df31993f7c
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 18:27:50 +0700

    filament level

--- app/Filament/Resources/Levels/Tables/LevelsTable.php
+++ app/Filament/Resources/Levels/Tables/LevelsTable.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Resources\Levels\Tables;
 
+use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\EditAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class LevelsTable
@@ -13,13 +15,23 @@ class LevelsTable
     {
         return $table
             ->columns([
-                //
-            ])
-            ->filters([
-                //
+                TextColumn::make('level_kode')
+                    ->label('Kode')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('level_nama')
+                    ->label('Nama Level')
+                    ->searchable()
+                    ->sortable(),
+
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d-m-Y H:i'),
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
