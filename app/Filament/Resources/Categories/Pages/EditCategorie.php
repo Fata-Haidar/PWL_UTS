3083b424e5c3448f8993268083f3049a58dce729
Fata-Haidar <fatahaidaraly@gmail.com>
2026-04-21 22:04:03 +0700

    filament kategori

--- app/Filament/Resources/Categories/Pages/EditCategorie.php
+++ app/Filament/Resources/Categories/Pages/EditCategorie.php
@@ -16,4 +16,8 @@ class EditCategorie extends EditRecord
             DeleteAction::make(),
         ];
     }
+        protected function getRedirectUrl(): string
+        {
+            return $this->getResource()::getUrl('index');
+        }
 }
